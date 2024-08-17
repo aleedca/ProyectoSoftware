be9a03aba8f6e54d2b8444cc9f7ca5e87e295ff7
deylan16 <deylan6@gmail.com>
2024-08-17 16:03:24 -0600

    conexion a base de batos

--- Codigo/Server/index.js
+++ Codigo/Server/index.js
@@ -2,22 +2,55 @@ const express = require('express')
 const app = express();
 const cors = require("cors");
 const db = require("./models");
+const sql = require("mssql");
 
 
 app.use(express.json());
 app.use(cors());
 
 //database
-db.sequelize.sync()
-    .then(() => {
-   
-      console.log("Base de datos conectada");
-    
-    })
-    .catch(error =>{
-        console.log("Error base de datos");
-    });
-  
+const config = {
+  user: "InloTEC",
+  password: "#$InloTEC1234",
+  server: "InloTEC.mssql.somee.com",
+  database: "InloTEC",
+  pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000,
+  },
+  options: {
+      encrypt: true,
+      trustServerCertificate: true,
+  },
+};
+
+async function connectAndQuery() {
+  const pool = new sql.ConnectionPool(config);
+
+  try {
+      // Conéctate a la base de datos
+      await pool.connect();
+      console.log("Conexión exitosa a la base de datos de Somee");
+
+      // Realiza la consulta
+      let result = await pool.request().query(`
+        SELECT TABLE_NAME
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_TYPE = 'BASE TABLE'
+      `);
+
+      // Muestra el resultado de la consulta
+      console.log(result.recordset);
+  } catch (error) {
+      console.error("Conexión fallida a la base de datos de Somee", error);
+  } finally {
+      // Cierra la conexión
+      pool.close();
+  }
+}
+
+connectAndQuery();
 
 
 // Routers
