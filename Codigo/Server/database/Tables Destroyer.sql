8fef071a303e85175366f8fa8f4db57908da2ede
Luis Fernando <luisfmp@estudiantec.cr>
2024-08-18 18:05:53 -0600

    Tables set-up and Tables destroyer

---
+++ Codigo/Server/database/Tables Destroyer.sql
@@ -0,0 +1,21 @@
+USE [InloTEC]
+GO
+
+DROP TABLE IF EXISTS [dbo].[Users_Roles];
+DROP TABLE IF EXISTS [dbo].[Users];
+
+DROP TABLE IF EXISTS [dbo].[Roles];
+DROP TABLE IF EXISTS [dbo].[Courses_Details];
+DROP TABLE IF EXISTS [dbo].[Courses];
+DROP TABLE IF EXISTS [dbo].[Programs];
+DROP TABLE IF EXISTS [dbo].[Locations];
+DROP TABLE IF EXISTS [dbo].[Teachers];
+DROP TABLE IF EXISTS [dbo].[ScheduleDays_Schedule];
+DROP TABLE IF EXISTS [dbo].[Schedule_Days];
+DROP TABLE IF EXISTS [dbo].[Schedule];
+DROP TABLE IF EXISTS [dbo].[Hours];
+DROP TABLE IF EXISTS [dbo].[Groups];
+DROP TABLE IF EXISTS [dbo].[Days];
+
+DROP TABLE IF EXISTS [dbo].[Change_Log];
+DROP TABLE IF EXISTS [dbo].[Errors];
