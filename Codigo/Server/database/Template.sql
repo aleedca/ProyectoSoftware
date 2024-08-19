49ce335fda650357b45ae9f491d93c6f53b78024
Luis Fernando <luisfmp@estudiantec.cr>
2024-08-18 18:05:07 -0600

    SQL template

---
+++ Codigo/Server/database/Template.sql
@@ -0,0 +1,73 @@
+--------------------------------------------------------------------------
+-- Author:      [name]
+-- Date:        [year]-[month]-[day]
+-- Description: [description]
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[Duende_SP_Name]
+    -- Parameters
+AS
+BEGIN
+    SET NOCOUNT ON;         -- No metadata returned
+
+    -- ERROR HANDLING
+    DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+    DECLARE @transactionBegun BIT = 0;
+
+    -- VARIABLE DECLARATION
+    -- 
+
+    BEGIN TRY
+
+        -- VALIDATIONS
+        --
+
+        -- TRANSACTION BEGUN
+        IF @@TRANCOUNT = 0
+        BEGIN
+            SET @transactionBegun = 1;
+            BEGIN TRANSACTION;
+        END;
+
+        --
+
+        -- TRANSACTION COMMITTED
+        IF @transactionBegun = 1
+        BEGIN
+            COMMIT TRANSACTION;
+        END;
+
+    END TRY
+    BEGIN CATCH
+
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transactionBegun = 1
+        BEGIN
+            ROLLBACK;
+        END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+            -- Non-custom errors are logged in the Errors table
+            INSERT INTO [dbo].[Errors]
+            VALUES (
+                SUSER_NAME(),
+                ERROR_NUMBER(),
+                ERROR_STATE(),
+                ERROR_SEVERITY(),
+                ERROR_LINE(),
+                ERROR_PROCEDURE(),
+                ERROR_MESSAGE(),
+                GETUTCDATE()
+            );
+        END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+
+    END CATCH;
+END;
