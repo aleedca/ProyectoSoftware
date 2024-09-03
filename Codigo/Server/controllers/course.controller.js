ff819774e8a0a989753021dac4125c57ac11bea2
Jennifer Alvarado Brenes <jeni12000@hotmail.com>
2024-09-03 04:56:49 -0600

    Add courses

--- Codigo/Server/controllers/course.controller.js
+++ Codigo/Server/controllers/course.controller.js
@@ -10,8 +10,30 @@ const getCourse = async (req, res) => {
 }
 
 const addCourse = async (req, res) => {
-    res.send("agregando un curso");
-}
+    try {
+        const pool = await getConnection();
+        console.log(req.body);
+
+        let result = await pool.request()
+            .input('IN_idCourses_Details', sql.BigInt, req.body['idDetalles'])
+            .input('IN_IdLocation', sql.BigInt, req.body['idUbicacion'])
+            .input('IN_IdTeachers', sql.BigInt, req.body['idProfesores'])
+            .input('IN_IdCourses', sql.BigInt, req.body['idCursos'])
+            .input('IN_IdSchedule', sql.BigInt, req.body['idHorario'])
+            .input('IN_IdModality', sql.BigInt, req.body['idModalidad'])
+            .input('IN_StartDate', sql.Date, req.body['fechaInicio'])
+            .input('IN_EndDate', sql.Date, req.body['fechaFinal'])
+            .input('IN_Notes', sql.NVarChar(512), req.body['notes'] || null) // Maneja valores NULL si no se proporcionan notas
+            .execute('InloTEC_SP_Courses_Details_Add'); // Nombre del procedimiento almacenado
+
+        console.log(result); // Muestra el resultado
+        res.status(200).send('Registro creado');
+    } catch (error) {
+        const errorMessage = error.message || 'Error desconocido';
+        console.error('Error al ejecutar el query:', errorMessage);
+        res.status(500).send(errorMessage);
+    }
+};
 
 const editCourse = async (req, res) => {
     res.send("editando un curso");
