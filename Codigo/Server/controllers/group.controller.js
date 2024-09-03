91a31153e5f74906b11597c51eadbfb12782887d
Jennifer Alvarado Brenes <jeni12000@hotmail.com>
2024-09-03 01:08:40 -0600

    Add groups

--- Codigo/Server/controllers/group.controller.js
+++ Codigo/Server/controllers/group.controller.js
@@ -10,7 +10,21 @@ const getGroup = async (req, res) => {
 }
 
 const addGroup = async (req, res) => {
-    res.send("agregando un grupo");
+    try {
+        const pool = await getConnection()
+        console.log(req.body)
+
+        let result = await pool.request()
+            .input('IN_name', sql.NVarChar(64), req.body['nombre'])
+            .execute('InloTEC_SP_Groups_Add'); // Nombre del procedimiento almacenado
+
+        console.log(result); // Muestra el resultado
+        res.status(200).send('Registro creado');
+    } catch (error) {
+        const errorMessage = error.message || 'Error desconocido';
+        console.error('Error al ejecutar el query:', errorMessage);
+        res.status(500).send(errorMessage);
+    }
 }       
 
 const editGroup = async (req, res) => {
