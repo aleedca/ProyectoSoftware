b4ee37b27cac26f5c365e2304975abaf637c6482
Luis Fernando <luisfmp@estudiantec.cr>
2024-09-03 18:38:33 -0600

    Operaions to get the catalog courses information

---
+++ Codigo/Server/database/Courses/InloTEC_SP_Courses_Get.sql
@@ -0,0 +1,72 @@
+-- Autor:       Luis Molina
+-- Fecha:       2024-09-1
+-- Descripci�n: Procedure to get a courses
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[InloTEC_SP_Courses_Get]
+	-- Parameters
+	@IN_IdCourses BIGINT
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- No metadata returned
+
+	-- ERROR HANDLING
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+	DECLARE @transactionBegun BIT = 0;
+
+	-- VARIABLE DECLARATION
+
+	BEGIN TRY
+        -- VALIDATIONS
+
+		-- Check if the id exists
+    	IF NOT EXISTS (SELECT 1
+					   FROM [dbo].[Courses] C
+					   WHERE C.Id = @IN_IdCourses
+					   AND Deleted = 0)
+    	BEGIN
+		RAISERROR('El curso indicado no existe. Por favor, utilice otro identificador.', 16, 1);
+		END;
+
+		--Response
+		SELECT C.Id, C.Name
+		FROM [dbo].[Courses] C
+		WHERE C.Id = @IN_IdCourses
+		AND Deleted = 0
+
+
+    END TRY
+    BEGIN CATCH
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transactionBegun = 1
+        BEGIN
+		ROLLBACK;
+	END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+		-- Non-custom errors are logged in the Errors table
+		INSERT INTO [dbo].[Errors]
+		VALUES
+			(
+				SUSER_NAME(),
+				ERROR_NUMBER(),
+				ERROR_STATE(),
+				ERROR_SEVERITY(),
+				ERROR_LINE(),
+				ERROR_PROCEDURE(),
+				ERROR_MESSAGE(),
+				GETUTCDATE()
+            );
+	END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+    END CATCH;
+END;
---
+++ Codigo/Server/database/Courses/InloTEC_SP_Courses_List.sql
@@ -0,0 +1,62 @@
+-- Autor:       Luis Molina
+-- Fecha:       2024-09-1
+-- Descripci�n: Procedure to list all the courses
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[InloTEC_SP_Courses_List]
+	-- Parameters
+	
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- No metadata returned
+
+	-- ERROR HANDLING
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+	DECLARE @transactionBegun BIT = 0;
+
+	-- VARIABLE DECLARATION
+
+	BEGIN TRY
+        -- VALIDATIONS
+
+		SELECT C.Id, C.Name
+		FROM Courses C
+		WHERE Deleted = 0
+		
+
+
+    END TRY
+    BEGIN CATCH
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transactionBegun = 1
+        BEGIN
+		ROLLBACK;
+	END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+		-- Non-custom errors are logged in the Errors table
+		INSERT INTO [dbo].[Errors]
+		VALUES
+			(
+				SUSER_NAME(),
+				ERROR_NUMBER(),
+				ERROR_STATE(),
+				ERROR_SEVERITY(),
+				ERROR_LINE(),
+				ERROR_PROCEDURE(),
+				ERROR_MESSAGE(),
+				GETUTCDATE()
+            );
+	END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+    END CATCH;
+END;
