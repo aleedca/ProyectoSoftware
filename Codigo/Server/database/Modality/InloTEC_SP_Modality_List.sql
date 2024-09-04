d6f578912635038c41d51aadc7b6b9079e8885ba
Luis Fernando <luisfmp@estudiantec.cr>
2024-09-03 18:40:06 -0600

    Operations to get the catalog modality information

---
+++ Codigo/Server/database/Modality/InloTEC_SP_Modality_List.sql
@@ -0,0 +1,65 @@
+-- Autor:       Luis Molina
+-- Fecha:       2024-09-1
+-- Descripci�n: Procedure to list all the modalities
+--------------------------------------------------------------------------
+
+CREATE OR ALTER PROCEDURE [dbo].[InloTEC_SP_Modality_List]
+	-- Parameters
+	
+
+AS
+BEGIN
+	SET NOCOUNT ON;
+	-- No metadata returned
+
+	-- ERROR HANDLING
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @Message VARCHAR(200);
+	DECLARE @transactionBegun BIT = 0;
+
+	-- VARIABLE DECLARATION
+
+	BEGIN TRY
+        -- VALIDATIONS
+
+
+		--Response
+		SELECT M.id, M.Name
+		FROM Modality M
+		WHERE Deleted = 0
+		
+		
+
+
+    END TRY
+    BEGIN CATCH
+        SET @ErrorNumber = ERROR_NUMBER();
+        SET @ErrorSeverity = ERROR_SEVERITY();
+        SET @ErrorState = ERROR_STATE();
+        SET @Message = ERROR_MESSAGE();
+
+        IF @transactionBegun = 1
+        BEGIN
+		ROLLBACK;
+	END;
+
+        IF @ErrorNumber != 50000
+        BEGIN
+		-- Non-custom errors are logged in the Errors table
+		INSERT INTO [dbo].[Errors]
+		VALUES
+			(
+				SUSER_NAME(),
+				ERROR_NUMBER(),
+				ERROR_STATE(),
+				ERROR_SEVERITY(),
+				ERROR_LINE(),
+				ERROR_PROCEDURE(),
+				ERROR_MESSAGE(),
+				GETUTCDATE()
+            );
+	END;
+
+        RAISERROR('%s - Error Number: %i', 
+            @ErrorSeverity, @ErrorState, @Message, @ErrorNumber);
+    END CATCH;
+END;
